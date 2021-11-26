8512d2a1b8b9b19b45d5d774b685ed8adc85f604
Patrick Kalies <patministrator@googlemail.com>
2021-11-26 19:54:03 +0100

    Login Sprachauswahl Formular

--- js/login/change_language.js
+++ js/login/change_language.js
@@ -1,6 +1,18 @@
 function change_language(lang){
-   
+  $.getJSON('lang/' + lang + '/loginform.json', function(loginform) {
+    //Header Card Sprachauswahl
+    $("#login_form_cardheader").html(loginform.lang_header)
+    //Erste Option - Sprache Dropdown - "Sprache auswählen!"
+    $("#lang_select").html(loginform.lang_select)
+    //Dropdown Option - Englisch
+    $("#lang_en").html(loginform.opt_englisch)
+    //Dropdown Option - Deutsch
+    $("#lang_de").html(loginform.opt_deutsch)
+    //Header Login-Formular
+    $("#loginform_header").html(loginform.loginFormLabel)
 
+
+  });
 }
 
 
