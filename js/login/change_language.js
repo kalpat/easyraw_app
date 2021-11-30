f0d746fc7184e0cd64ca52d99cfb6fee20c50023
Patrick Kalies <patministrator@googlemail.com>
2021-11-30 21:59:09 +0100

    Login-Form mit Language-Selection steht. Sprache wechselt, aber der Rest ist ohne Funktion.

--- js/login/change_language.js
+++ js/login/change_language.js
@@ -1,6 +1,10 @@
 function change_language(lang){
   $.getJSON('lang/' + lang + '/loginform.json', function(loginform) {
-    //Header Card Sprachauswahl
+    //Email-Msg-Feld unsichtbar machen
+    $("#email_msg").hide();
+    //Password-Msg-Feld unsichtbar machen
+    $("#password_msg").hide();
+   //Header Card Sprachauswahl
     $("#login_form_cardheader").html(loginform.lang_header)
     //Erste Option - Sprache Dropdown - "Sprache auswählen!"
     $("#lang_select").html(loginform.lang_select)
@@ -10,14 +14,24 @@ function change_language(lang){
     $("#lang_de").html(loginform.opt_deutsch)
     //Header Login-Formular
     $("#loginform_header").html(loginform.loginFormLabel)
-
-
+    //Label Username
+    $("#L_usermail").html(loginform.usermail)
+    //Label Password
+    $("#L_password").html(loginform.password)
+    //Button - Login
+    $("#login_button").html(loginform.login_button)
+    //Button - Account erstellen
+    $("#registerform_button").html(loginform.registerform_button)
   });
 }
 
-
-
-
-
   //Funktion - Sprachausgabe - Login-Formular
   change_language(lang);
+
+  $('#language_selection').change(function(){
+    var get_value = $(this).val();
+    localStorage.setItem("lang", get_value);
+    var set_lang = localStorage.getItem("lang");
+    change_language(set_lang)
+  });
+
--- js/login/login_form.js
+++ js/login/login_form.js
@@ -2,7 +2,6 @@
 //Wenn in localStorage keine verschlüsselte Mail-Adresse vorhanden ist,
   //dann müssen alle Menü ausgeblendet werden und das Login-Formular geladen
   if (localStorage.getItem("usermail_encrypted")===null) { 
-
     //Login-Formular wird geladen und angezeigt
     $("#App_Content").load("templates/loginform.html");
     $.getScript("./js/login/change_language.js");
