1002db232831d8115422158c5bbe14315a018c9d
pkalies <52819381+pkalies@users.noreply.github.com>
2021-12-04 22:30:42 +0100

    Registrierung Formular - vollständig

---
+++ js/registration/registerform_lang.js
@@ -0,0 +1,22 @@
+function registerform_lang(lang){
+
+    $.getJSON('lang/' + lang + '/registrationform.json', function(registerform) {
+        console.log(registerform);
+  //Label - Registrierungsformular - card header
+      $("#registerform_header").html(registerform.registerFormLabel)
+      //Label Input-Field Usermail
+      $("#L_usermail").html(registerform.usermail)
+      //Label input-Field - Password
+      $("#L_password").html(registerform.password)
+      //Label Input-Field - Password Confirmation
+      $("#L_password_confirm").html(registerform.password_confirm)
+      //Label - Button - Account erstellen
+      $("#create_button").html(registerform.create_account)
+      //Label - Button - Cancel
+      $("#cancel_button").html(registerform.cancel_creation)
+    });
+  }
+
+  var lang = localStorage.getItem("lang");
+  console.log('Registerform Lang läuft eigentlich');
+    registerform_lang(lang);
--- js/registration/registration_form.js
+++ js/registration/registration_form.js
@@ -1,4 +1,9 @@
 $('#registerform_button').click(function() {
+  console.debug("TEst");
     $("#App_Content").load("templates/registrationform.html");
+
+    $.getScript("./js/registration/registerform_lang.js");
+    
   });
+
 
